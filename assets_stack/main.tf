b73407d9493ec5dd81de7664c4eaa7addd4e551f
HeNeos <heneos3.14@gmail.com>
2024-04-21 23:48:15 +0200

    Include cloudfront distribution
    TODO: Add aliases (not needed at this moment)

--- assets_stack/main.tf
+++ assets_stack/main.tf
@@ -66,3 +66,58 @@ resource "aws_s3_bucket_website_configuration" "problems_bucket_website_configur
     suffix = "index.html"
   }
 }
+
+locals {
+  s3_origin_id = "S3-${aws_s3_bucket.problems_bucket.bucket}"
+}
+
+resource "aws_cloudfront_distribution" "brilliant_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.problems_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Brilliant index distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 15
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "BrilliantDistribution"
+  }
+}
